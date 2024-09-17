f8a046d3569ed08b4fc4fbdf09b1f75c9ebee60a
ajay2512 <bhakarajay033@gmail.com>
2024-09-17 14:28:08 +0530

    container,images,modes done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          docker pdh rhe hai bhai !!!!!!
         </a>
       </header>
     </div>
